e00e463b150d19be9896c9cf4a2f9361e3e1fe6b
Alexander M <hellomedici@outlook.com>
2025-12-05 11:31:29 -0500

    added readme

--- backend/server.js
+++ backend/server.js
@@ -33,15 +33,21 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
-    res.send("Backend API is running");
+    res.send("API is running....");
   });
 }
+
 app.use(notFound);
 app.use(errorHandler);
 
